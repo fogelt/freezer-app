b39252cb4de65df9a1af6dbc1de074c66cd1e56e
fogelt <fogeltt@gmail.com>
2026-04-19 12:10:03 +0200

    clean up

--- src/Login.tsx
+++ src/Login.tsx
@@ -6,8 +6,6 @@ export default function Login() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [loading, setLoading] = useState(false)
-  const [isShowing, setIsShowing] = useState(false)
-  const [message, setMessage] = useState('HEJ')
 
   async function handleLogin(e: React.FormEvent) {
     e.preventDefault()
@@ -17,28 +15,9 @@ export default function Login() {
     setLoading(false)
   }
 
-  const handlePress = () => {
-    setIsShowing(!isShowing)
-    setMessage('Pressed')
-  }
-
   return (
     <div className="flex min-h-screen bg-slate-50 font-sans text-slate-900">
 
-      {/* SIDEBAR CONTAINER */}
-      <div
-        className={`
-        overflow-hidden duration-500
-        ${isShowing ? 'w-64' : 'w-0'}
-      `}
-      >
-        <div className="w-64 p-6 bg-slate-300 h-full">
-          <p>{message}</p>
-        </div>
-      </div>
-
-      {/* MODAL CONTAINER */}
-      {/* flex-1 ensures this div takes up all remaining space */}
       <div className="flex-1 flex items-center justify-center p-6">
         <div className="w-full max-w-md rounded-2xl bg-white p-8 shadow-xl border border-slate-100">
           <h1 className="text-4xl font-black tracking-tighter text-brand text-center pb-4">
